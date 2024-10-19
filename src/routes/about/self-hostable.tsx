e4ef20d67d746d189c566d9faee9a8070d853dd8
yermie123 <yermolovpersonal@gmail.com>
2024-10-18 21:13:20 -0700

    Fixed self-hosted and fixed zero-knowledge but both need revision

--- src/routes/about/self-hostable.tsx
+++ src/routes/about/self-hostable.tsx
@@ -1,7 +1,7 @@
 import type { Component } from "solid-js";
 import { Title } from "@solidjs/meta";
 import { A } from "@solidjs/router";
-import { createSignal, onMount, Ref } from "solid-js";
+import { createSignal, onMount, Ref, Show } from "solid-js";
 import NavBar from "~/components/NavBar";
 import Footer from "~/components/Footer";
 import "./about.scss"
@@ -21,6 +21,16 @@ const SelfHosted: Component = () => {
   let coder: any;
   let notcoder: any;
 
+  const changeExplanation = (explanation: boolean) => {
+    if(explanation) {
+      localStorage.setItem("explanation", "coder");
+      setExplanation(true);
+    } else if (!explanation) {
+      localStorage.setItem("explanation", "notcoder");
+      setExplanation(false);
+    }
+  }
+
     return(
         <main>
             <Title>CipherCrypt Demo - About</Title>
@@ -29,42 +39,72 @@ const SelfHosted: Component = () => {
                 <div id="sidebar-section">
                     <div id="sidebar-options" class="radio-input">
                         <div class="radio-section">
-                        <input ref={coder} type="radio" id="coder" value="coder" checked name="explanation" onChange={() => localStorage.setItem("explanation", "coder")} />
-                        <label for="coder">I have Coding Experience</label>
+                            <input ref={coder} type="radio" id="coder" value="coder" checked name="explanation" onChange={() => changeExplanation(true)} />
+                            <label for="coder">I have Coding Experience</label>
                         </div>
                         <div class="radio-section">
-                        <input ref={notcoder} type="radio" id="notcoder" value="notcoder" name="explanation" onChange={() => localStorage.setItem("explanation", "notcoder")} />
-                        <label for="notcoder">I want the Simple Explanation</label>
+                            <input ref={notcoder} type="radio" id="notcoder" value="notcoder" name="explanation" onChange={() => changeExplanation(false)} />
+                            <label for="notcoder">I want the Simple Explanation</label>
                         </div>
                     </div>
                     <div id="sidebar">
                         <A href="/about"><h1>Core Functionalities</h1></A>
                         <hr />
                         <A href="/about/zero-knowledge"><h2>Zero Knowledge Encryption</h2></A>
-                        <h2>Self Hosted</h2>
-                        <h2>Easy Setup</h2>
-                        <h2>MongoDB Support</h2>
-                        <h2>Core Technologies</h2>
-                        <h2>Caching</h2>
+                        <A href="/about/self-hostable"><h2>Self Hosted</h2></A>
+                        <A href="/about/no-experience"><h2>Easy Setup</h2></A>
+                        <A href="/about/mongodb"><h2>MongoDB Support</h2></A>
+                        <A href="/about/technologies"><h2>Core Technologies</h2></A>
+                        <A href="/caching"><h2>Caching</h2></A>
                     </div>
                 </div>
-                <div class="article">
-                    <h2>CipherCrypt is Self-Hosted</h2>
-                    <p>
-                        This is not a system for profit, but rather for any use by anyone who wants to use it.
-                    </p>
-                    <p>
-                        More importantly, this is a proof of concept, rather than a dedicated project. I, as the creator of this project, intend to continue working on CipherCrypt to improve and innovate it.
-                        That being said, there is a lot of responsibility in handling secret data. Some of the responsibility can be mitigated by resources I might not have access to.
-                        Nevertheless I can show a concept of something that works as a better alternative to what many use as a 'storage' of passwords on their devices.
-                    </p>
-                    <p>
-                        Another key reason for this is that self-hosting such a project makes it more personal. It is yours to tinker and experiment with.
-                    </p>
-                    <p>
-                        And I will do my best in outlining how to set up the project properly.
-                    </p>
-                </div>
+                <Show when={explanation()} fallback={
+                    <div class="article">
+                        <h2>CipherCrypt is Self-Hosted</h2>
+                        <p>
+                            There is no pre-set version for you to use or pay for.
+                            There is a demo that you may feel free to try, but the project is self-hosted only.
+                        </p>
+                        <p>
+                            What this means is that you will need to set it up yourself, for which there is a step-by-step guide.
+                        </p>
+                        <h3>Why?</h3>
+                        <p>
+                            For starters, it is not a system for profit, and self hosting this is taking ownership of your own instance of this project.
+                        </p>
+                        <p>
+                            Additionally, there are differences in different data-encryption products offered by various companies with their own pros and cons, and this project comes with its own drawbacks. 
+                        </p>
+                        <p>
+                            While some companies out there create password managers that rely on good hardware, they sometimes use weak software.
+                            Good companies will use both, good hardware and good software. There are many good companies in this sphere that offer extremely good products.
+                            CipherCrypt is a proof of concept, even though I plan on improving and innovating the project.
+                        </p>
+                        <p>    
+                            But, the biggest weakness is in how it is implemented.
+                            For instance, many ecryption products rely on expensive hardware.
+                            But CipherCrypt is self-hosted, and you can implement it with any hardware you want, or use an online instance of MongoDB.
+                        </p>
+                    </div>
+                }>
+                    <div class="article">
+                        <h2>CipherCrypt is Self-Hosted</h2>
+                        <p>
+                            This is not a system for profit, but rather for any use by anyone who wants to use it.
+                        </p>
+                        <p>
+                            More importantly, this is a proof of concept, rather than a dedicated project. I, as the creator of this project, intend to continue working on CipherCrypt to improve and innovate it.
+                            That being said, there is a lot of responsibility in handling secret data. Some of the responsibility can be mitigated by resources I might not have access to.
+                            Nevertheless I can show a concept of something that works as a better alternative to what many use as a 'storage' of passwords on their devices.
+                        </p>
+                        <p>
+                            Another key reason for this is that self-hosting such a project makes it more personal. It is yours to tinker and experiment with.
+                        </p>
+                        <p>
+                            And I will do my best in outlining how to set up the project properly.
+                        </p>
+                    </div>
+                </Show>
             </div>
             <Footer />
         </main>
--- src/routes/about/zero-knowledge.tsx
+++ src/routes/about/zero-knowledge.tsx
@@ -1,7 +1,7 @@
 import type { Component } from "solid-js";
 import { Title } from "@solidjs/meta";
 import { A } from "@solidjs/router";
-import { createSignal, onMount, Ref } from "solid-js";
+import { createSignal, onMount, Ref, Show } from "solid-js";
 import NavBar from "~/components/NavBar";
 import Footer from "~/components/Footer";
 import "./about.scss"
@@ -18,6 +18,16 @@ const ZeroKnowledge: Component = () => {
     }
   })
 
+  const changeExplanation = (explanation: boolean) => {
+    if(explanation) {
+      localStorage.setItem("explanation", "coder");
+      setExplanation(true);
+    } else if (!explanation) {
+      localStorage.setItem("explanation", "notcoder");
+      setExplanation(false);
+    }
+  }
+
   let coder: any;
   let notcoder: any;
 
@@ -29,11 +39,11 @@ const ZeroKnowledge: Component = () => {
                 <div id="sidebar-section">
                     <div id="sidebar-options" class="radio-input">
                         <div class="radio-section">
-                        <input ref={coder} type="radio" id="coder" value="coder" checked name="explanation" onChange={() => localStorage.setItem("explanation", "coder")} />
+                        <input ref={coder} type="radio" id="coder" value="coder" checked name="explanation" onChange={() => changeExplanation(true)} />
                         <label for="coder">I have Coding Experience</label>
                         </div>
                         <div class="radio-section">
-                        <input ref={notcoder} type="radio" id="notcoder" value="notcoder" name="explanation" onChange={() => localStorage.setItem("explanation", "notcoder")} />
+                        <input ref={notcoder} type="radio" id="notcoder" value="notcoder" name="explanation" onChange={() => changeExplanation(false)} />
                         <label for="notcoder">I want the Simple Explanation</label>
                         </div>
                     </div>
@@ -41,58 +51,81 @@ const ZeroKnowledge: Component = () => {
                         <A href="/about"><h1>Core Functionalities</h1></A>
                         <hr />
                         <A href="/about/zero-knowledge"><h2>Zero Knowledge Encryption</h2></A>
-                        <h2>Self Hosted</h2>
-                        <h2>Easy Setup</h2>
-                        <h2>MongoDB Support</h2>
-                        <h2>Core Technologies</h2>
-                        <h2>Caching</h2>
+                        <A href="/about/self-hostable"><h2>Self Hosted</h2></A>
+                        <A href="/about/no-experience"><h2>Easy Setup</h2></A>
+                        <A href="/about/mongodb"><h2>MongoDB Support</h2></A>
+                        <A href="/about/technologies"><h2>Core Technologies</h2></A>
+                        <A href="/caching"><h2>Caching</h2></A>
                     </div>
                 </div>
-                <div class="article">
-                    <p>
-                        Zero-Knowledge encryption is an encryption method that uses a key that cannot be accessed by the program.
-                        In other words, when the data you enter into the program is encrypted, CipherCrypt does not have any backdoor access to the data.
-                    </p>
-                    <h3>
-                        How Does it Work?
-                    </h3>
-                    <p>
-                        The method used by CipherCrypt is the same method used by many other companies in tech, especially those that value open source (BitWarden, Proton, etc.).
-                        We simply use the password before hashing it.
-                    </p>
-                    <p>
-                        Every <b>secure</b> system out there hashes your password when storing it in the database.
-                        Let's say you sign up for an email. And let's say the password you create is 'secret123'.
-                        The email provider/company uses some sort of algorithm for every password in their system. Suppose they use SHA256, which is a well known algorithm developed by the NSA.
-                        When the email provider plugs in 'secret123' into the algorithm, they get the following:
-                    </p>
-                    <p>
-                        <b>fcf730b6d95236ecd3c9fc2d92d7b6b2bb061514961aec041d6c7a7192f592e4</b>
-                    </p>
-                    <p>
-                        Looks like a bunch of gibberish, right? But this gibberish is extremely important, since it's virtually impossible to 'decrypt' the hash back into the origin password.
-                        Hackers don't even try to reverse-engineer the phrase, they use other means of trying to guess your password.
-                    </p>
-                    <h3>
-                        So, how do you Log In?
-                    </h3>
-                    <p>
-                        Every time you log in, the attempted log in password is generated into a new hash, and the new hash is compared to the one stored in the database of the email provider.
-                        When you attempt to log in, let's say you misspelled the password and said 'secret124'. A new hash is generated and the result is <b>2f4350974680903805ab7da16a7fbe0bd358b31d7560544b82c5b6870110b2db</b>
-                        Notice how this hash is different from the one mentioned earlier? Your email provider does to, and will tell you that the password entered is incorrect.
-                    </p>
-                    <p>
-                        Also, in case you ever wondered, this is the same reason why your email can't simply tell you what your password was when you forgot it, but they still know if you're entering your old password because they have the old hash.
-                    </p>
-                    <p>
-                        But the trick in Zero-Knowledge encryption lies in what happens before the hashing.
-                        Unlike hashing (only needs an algorithm and something to hash), encryption also needs a key, because it needs to be descrypted to access the data at some point.
-                        So, when CipherCrypt generates a hash for your password 'secret123' to store the hash in the database, the plain-text version of your password (secret123) is used as the key to encrypt your data.
-                    </p>
-                    <p>
-                        Using this method is the simplest way to achieve Zero-Knowledge encryption.
-                    </p>
-                </div>
+                <Show when={explanation()}
+                    fallback={
+                    <div class="article">
+                        <h2>Zero-Knowledge Encryption</h2>
+                        <p>
+                            Zero-Knowledge encryption is an encryption method that uses a key that cannot be accessed by the program.
+                            In other words, when the data you enter into the program is encrypted, CipherCrypt does not have any backdoor access to the data.
+                        </p>
+                        <h3>
+                            How Does it Work?
+                        </h3>
+                        <p>
+                            The method used by CipherCrypt is the same method used by many other companies in tech, especially those that value open source (BitWarden, Proton, etc.).
+                            We simply use the password before hashing it.
+                        </p>
+                        <p>
+                            Every <b>secure</b> system out there hashes your password when storing it in the database.
+                            Let's say you sign up for an email. And let's say the password you create is 'secret123'.
+                            The email provider/company uses some sort of algorithm for every password in their system. Suppose they use SHA256, which is a well known algorithm developed by the NSA.
+                            When the email provider plugs in 'secret123' into the algorithm, they get the following:
+                        </p>
+                        <p>
+                            <b>fcf730b6d95236ecd3c9fc2d92d7b6b2bb061514961aec041d6c7a7192f592e4</b>
+                        </p>
+                        <p>
+                            Looks like a bunch of gibberish, right? But this gibberish is extremely important, since it's virtually impossible to 'decrypt' the hash back into the origin password.
+                            Hackers don't even try to reverse-engineer the phrase, they use other means of trying to guess your password.
+                        </p>
+                        <h3>
+                            So, how do you Log In?
+                        </h3>
+                        <p>
+                            Every time you log in, the attempted log in password is generated into a new hash, and the new hash is compared to the one stored in the database of the email provider.
+                            When you attempt to log in, let's say you misspelled the password and said 'secret124'. A new hash is generated and the result is <b>2f4350974680903805ab7da16a7fbe0bd358b31d7560544b82c5b6870110b2db</b>
+                            Notice how this hash is different from the one mentioned earlier? Your email provider does to, and will tell you that the password entered is incorrect.
+                        </p>
+                        <p>
+                            Also, in case you ever wondered, this is the same reason why your email can't simply tell you what your password was when you forgot it, but they still know if you're entering your old password because they have the old hash.
+                        </p>
+                        <p>
+                            But the trick in Zero-Knowledge encryption lies in what happens before the hashing.
+                            Unlike hashing (only needs an algorithm and something to hash), encryption also needs a key, because it needs to be descrypted to access the data at some point.
+                            So, when CipherCrypt generates a hash for your password 'secret123' to store the hash in the database, the plain-text version of your password (secret123) is used as the key to encrypt your data.
+                        </p>
+                        <p>
+                            Using this method is the simplest way to achieve Zero-Knowledge encryption.
+                        </p>
+                    </div>
+                    }>
+                    <div class="article">
+                        <h2>Zero Knowledge Encryption</h2>
+                        <p>
+                            CipherCrypt uses the most popular way of implementing Zero-Knowledge Encryption, by using the password as the encryption key before hashing it and storing in the database.
+                        </p>
+                        <p>
+                            This method is simple and effective, and is used by many companies out there (BitWarden, Proton, etc.).
+                        </p>
+                        <p>
+                            Zero-Knowledge encryption is important, even if you are hosting CipherCrypt on your own server.
+                            Aside from the fact that you might entrust your server to other people for use (and future development might even encourage this),
+                            zero-knowledge encryption is useful against many security threats. If no one but the account user can decrypt their own data,
+                            that is a layer of protection against malicious actors.
+                        </p>
+                        <p>
+                            For more information, see: <a href="https://en.wikipedia.org/wiki/Zero-knowledge_protocol">Wikipedia</a>
+                        </p>
+                    </div>
+                </Show>
             </div>
             <Footer />
         </main>
