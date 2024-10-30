b3554c4291e6fd89aaf54e1993a5e1b930ed13ac
yermie123 <yermolovpersonal@gmail.com>
2024-10-30 02:40:29 -0700

    As ugly as its plainless (of the home page) looks, it will do for now

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,64 @@
 import { A } from "@solidjs/router";
 import type { Component } from "solid-js";
+import { Show, createSignal, onMount, Ref } from "solid-js";
 import "./NavBar.scss";
 
 const NavBar: Component = () => {
+    const [windowWidth, setWindowWidth] = createSignal(0);
+
+    onMount(() => {
+        if(typeof window !== "undefined") {
+            setWindowWidth(window.innerWidth);
+        }
+    })
+
+    const toggleMenuOn = () => {
+        menuOverlayRef.style.display = "block";
+        menuOverlayRef.style.zIndex = "5";
+        menuModalRef.show();
+        menuModalRef.style.overflow = "hidden";
+    }
+
+    const toggleMenuOff = () => {
+        menuOverlayRef.style.display = "none";
+        menuOverlayRef.style.zIndex = "-1";
+        menuModalRef.close();
+        menuModalRef.style.overflow = "visible";
+    }
+
+    let menuOverlayRef: any;
+    let menuModalRef: any;
+
     return (
         <nav>
             <section id="nav-left" class="nav-section">
                 <A href="/" class="nav-el">Ciphercrypt</A>
             </section>
             <section id="nav-right" class="nav-section">
-                <A href="/demo" class="nav-el">Demo</A>
-                <A href="/about" class="nav-el">About</A>
+                <Show when={windowWidth() > 500}
+                fallback={
+                    <img src="/menu.svg" id="menu-icon" onClick={() => toggleMenuOn()} />
+                }>
+                    <A href="/demo" class="nav-el">Demo</A>
+                    <A href="/about" class="nav-el">About</A>
+                </Show>
             </section>
+            <div id="overlay" ref={menuOverlayRef} onClick={(e: any) => {if(e.target === menuOverlayRef) toggleMenuOff()}}>
+                <dialog ref={menuModalRef}>
+                    <A href="/" class="nav-el">Ciphercrypt</A>
+                    <hr id="menu-divider" />
+                    <A href="/demo" class="nav-el">Demo</A>
+                    <A href="/about" class="nav-el">About</A>
+                    <ul>
+                        <A href="/about/zero-knowledge"><li>Zero-Knowledge Encryption</li></A>
+                        <A href="/about/no-experience"><li>Easy Setup</li></A>
+                        <A href="/about/self-hostable"><li>Self-Hosted</li></A>
+                        <A href="/about/technologies"><li>Technologies</li></A>
+                        <A href="/about/mongodb"><li>MongoDB</li></A>
+                        <A href="/about/caching"><li>Caching</li></A>
+                    </ul>
+                </dialog>
+            </div>
         </nav>
     )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Title } from "@solidjs/meta";
 import { A } from "@solidjs/router";
-import { onMount, Ref } from "solid-js";
+import { onMount, Ref, createSignal } from "solid-js";
 import './index.scss'
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer"
 // import lock4_1 from "/lock4_1.png";
 
 export default function Home() {
+  const [initialText, setInitialText] = createSignal<string>("CipherCrypt");
+
   onMount(() => {
     blinker1.innerText = "|";
     typeWriter("CipherCrypt", headerRef1, blinker1)
@@ -47,6 +49,8 @@ export default function Home() {
         {/* <section id="hero">
           <img src="/lock3.png" class="min-w-44" />
         </section> */}
+        <A href="/demo" id="demo-title">Check out the Demo</A>
+        <hr class="divider" />
         <h2 id="features-title">Key Features</h2>
         <section id="features">
 
@@ -54,54 +58,54 @@ export default function Home() {
             <div class="first-content" id="feature1">
               <h3>Zero-knowledge encryption</h3>
             </div>
-            <div class="second-content">
+            <A href="/about/zero-knowledge" class="second-content">
               <p>Only you have access to your passwords. We can't read them, and neither can anyone else.</p>
-            </div>
+            </A>
           </div>
 
           <div class="card">
             <div class="first-content" id="feature2">
               <h3>Self-hostable, free for anyone</h3>
             </div>
-            <div class="second-content">
+            <A href="/about/self-hostable" class="second-content">
               <p>Host Ciphercrypt on your own server, or use our free hosting option. No credit card required.</p>
-            </div>
+            </A>
           </div>
 
           <div class="card">
             <div class="first-content" id="feature3">
               <h3>Easy setup, no code experience needed</h3>
             </div>
-            <div class="second-content">
+            <A href="/about/no-experience" class="second-content">
               <p>Get started in minutes, without needing to know how to code.</p>
-            </div>
+            </A>
           </div>
 
           <div class="card">
             <div class="first-content" id="feature4">
               <h3>MongoDB support (SQL integration planned)</h3>
             </div>
-            <div class="second-content">
+            <A href="/about/mongodb" class="second-content">
               <p>Store your passwords securely in MongoDB, with plans for SQL integration in the future.</p>
-            </div>
+            </A>
           </div>
 
           <div class="card">
             <div class="first-content" id="feature5">
               <h3>Built with Go, Next.js, Argon2, and AES 256</h3>
             </div>
-            <div class="second-content">
+            <A href="/about/technologies" class="second-content">
               <p> Ciphercrypt uses modern, secure technologies to keep your passwords safe.</p>
-            </div>
+            </A>
           </div>
 
           <div class="card">
             <div class="first-content" id="feature6">
               <h3>Redis caching for faster performance</h3>
             </div>
-            <div class="second-content">
+            <A href="/about/caching" class="second-content">
               <p>Get fast access to your passwords with Redis caching.</p>
-            </div>
+            </A>
           </div>
 
         </section>
