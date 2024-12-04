73d627656df12dcc42b66342c5c05b709c1ab016
yermie123 <yermolovpersonal@gmail.com>
2024-12-03 23:59:30 -0800

    Started d3 work

--- src/components/Demo/DecriptionAnimation.tsx
+++ src/components/Demo/DecriptionAnimation.tsx
@@ -1,5 +1,7 @@
 import { Ref, createSignal, onMount } from "solid-js";
 import type { Component } from "solid-js";
+// @ts-ignore
+import * as d3 from "d3";
 
 const DecriptionAnimation: Component<{
   encryptedData: any;
@@ -12,6 +14,43 @@ const DecriptionAnimation: Component<{
     enc = JSON.parse(localStorage.getItem("encData")!);
   if (props.decryptedData.plaintext === "")
     dec = JSON.parse(localStorage.getItem("decData")!);
+
+  // // Set up the SVG
+  // const svg = d3
+  //   .select("body")
+  //   .append("svg")
+  //   .attr("width", 800)
+  //   .attr("height", 600);
+
+  // // Prepare the block data
+  // const blockSize = 128; // bits
+  // const numBlocks = 10;
+  // const blockData = Array(numBlocks)
+  //   .fill(0)
+  //   .map((_, i) => ({
+  //     id: i,
+  //     encrypted: `Block ${i} (Encrypted)`,
+  //     decrypted: `Block ${i} (Decrypted)`,
+  //   }));
+
+  // // Draw the block grid
+  // const blocks = svg
+  //   .selectAll("rect")
+  //   .data(blockData)
+  //   .enter()
+  //   .append("rect")
+  //   .attr("x", (d, i) => i * 50)
+  //   .attr("y", 50)
+  //   .attr("width", 40)
+  //   .attr("height", 40)
+  //   .attr("fill", "gray");
+
+  // // Animate the decryption process
+  // blocks
+  //   .transition()
+  //   .duration(2000)
+  //   .attr("fill", "green")
+  //   .text((d) => d.decrypted);
 
   return (
     <div id="decription-animation">
