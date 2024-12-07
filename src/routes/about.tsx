7572dda389df1c22c8b2349218d972581d6b9bcd
yermie123 <yermolovpersonal@gmail.com>
2024-12-07 01:09:01 -0800

    Started work on refactoring and restyling about section

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -8,11 +8,56 @@ import Footer from "~/components/Footer";
 import "./about.scss";
 
 export default function AboutLayout(props: RouteSectionProps) {
+  // TODO: Control selection of readability from here
+
+  // TODO: Make the following style change to active links:
+  // cache.style.color = "#b2dcc9";
+  // cache.style.textDecoration = "underline";
+
   return (
     <main>
       <Title>About - CipherCrypt</Title>
       <NavBar />
-      {props.children}
+      <div id="inner-main">
+        <div id="sidebar-section">
+          <A href="/about/about-general" class="pseudo button">
+            <h2>About CipherCrypt</h2>
+          </A>
+          <hr />
+          <div id="sidebar-tabs">
+            <A
+              href="/about/zero-knowledge"
+              id="zk"
+              class="toggle pseudo button"
+            >
+              Zero-knowledge encryption
+            </A>
+            <A href="/about/self-hostable" id="sh" class="toggle pseudo button">
+              Self-hosted
+            </A>
+            <A href="/about/no-experience" id="gs" class="toggle pseudo button">
+              Guided Setup: No Experience Required
+            </A>
+            <A href="/about/caching" id="cache" class="toggle pseudo button">
+              Built-in Caching
+            </A>
+            <A
+              href="/about/technologies"
+              id="tech"
+              class="toggle pseudo button"
+            >
+              Powerful Technologies
+            </A>
+            <A href="/about/mongodb" id="mdb" class="toggle pseudo button">
+              MongoDB support
+            </A>
+            <A href="/about/about-demo" id="demo" class="toggle pseudo button">
+              About Demo
+            </A>
+          </div>
+        </div>
+        {props.children}
+      </div>
       <Footer />
     </main>
   );
--- src/routes/about/about-general.tsx
+++ src/routes/about/about-general.tsx
@@ -6,56 +6,28 @@ import { createSignal, onMount, Ref, Show } from "solid-js";
 
 export default function AboutGeneral() {
   return (
-    <div id="inner-main">
-      <div id="sidebar-section">
-        <A href="/about/about-general" class="pseudo button">
-          <h2>About CipherCrypt</h2>
-        </A>
-        <hr />
-        <div id="sidebar-tabs">
-          <A href="/about/zero-knowledge" id="zk" class="toggle pseudo button">
-            Zero-knowledge encryption
-          </A>
-          <A href="/about/self-hostable" id="sh" class="toggle pseudo button">
-            Self-hosted
-          </A>
-          <A href="/about/no-experience" id="gs" class="toggle pseudo button">
-            Guided Setup: No Experience Required
-          </A>
-          <A href="/about/caching" id="cache" class="toggle pseudo button">
-            Built-in Caching
-          </A>
-          <A href="/about/technologies" id="tech" class="toggle pseudo button">
-            Powerful Technologies
-          </A>
-          <A href="/about/mongodb" id="mdb" class="toggle pseudo button">
-            MongoDB support
-          </A>
-        </div>
-      </div>
-      <div class="article" id="main-descriptor">
-        <p>
-          <b>CipherCrypt</b> is an attempt to create a simple alternative to
-          storing passwords in plain text on your notepad or using the same
-          password for everything.
-        </p>
-        <p>
-          This project uses technologies in an efficient and effective way.
-          However, although CipherCrypt is free, and uses top of the line
-          algorithms and technologies, it comes with its drawbacks.
-        </p>
-        <p>
-          CipherCrypt is self-hosted, and uses MongoDB as the database. It's
-          that simple. However, this also means that it doesn't require any
-          expensive hardware that could come with its own security.
-        </p>
-        <p>
-          As you navigate through the other articles, notice that many will have
-          an option to select whether you have an experience with coding or not.
-          This is done in hopes that anyone can understand how CipherCrypt works
-          and the issue it's trying to solve.
-        </p>
-      </div>
+    <div class="article" id="main-descriptor">
+      <p>
+        <b>CipherCrypt</b> is an attempt to create a simple alternative to
+        storing passwords in plain text on your notepad or using the same
+        password for everything.
+      </p>
+      <p>
+        This project uses technologies in an efficient and effective way.
+        However, although CipherCrypt is free, and uses top of the line
+        algorithms and technologies, it comes with its drawbacks.
+      </p>
+      <p>
+        CipherCrypt is self-hosted, and uses MongoDB as the database. It's that
+        simple. However, this also means that it doesn't require any expensive
+        hardware that could come with its own security.
+      </p>
+      <p>
+        As you navigate through the other articles, notice that many will have
+        an option to select whether you have an experience with coding or not.
+        This is done in hopes that anyone can understand how CipherCrypt works
+        and the issue it's trying to solve.
+      </p>
     </div>
   );
 }
